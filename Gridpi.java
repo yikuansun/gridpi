ea362c81e965f99e9bc0cb7db51e7dfcdad583e4
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-12-21 11:34:25 -0500

    Make the thing

--- Gridpi.java
+++ Gridpi.java
@@ -1,5 +1,5 @@
 public class Gridpi {
-    private int gaps;
+    private int gaps, totalPts, targetPts;
 
     public Gridpi(int gaps) {
         this.setGaps(gaps);
@@ -7,5 +7,23 @@ public class Gridpi {
 
     public void setGaps(int gaps) {
         this.gaps = gaps;
+    }
+
+    public void calculatePts() {
+        for (double y = -1.0; y <= 1.0; y += 1.0 / this.gaps) {
+            for (double x = -1.0; x <= 1.0; x += 1.0 / this.gaps) {
+                this.totalPts++;
+                if (Math.pow(x, 2) + Math.pow(y, 2) <= 1.0) this.targetPts++;
+            }
+        }
+    }
+
+    public int getTotalPts() { return this.totalPts; }
+
+    public int getTargetPts() { return this.targetPts; }
+
+    public double getPi() {
+        this.calculatePts();
+        return 4.0 * this.getTargetPts() / this.getTotalPts();
     }
 }
--- Main.java
+++ Main.java
@@ -7,5 +7,8 @@ class Main {
         System.out.print("Iteration size: 1/");
         int response = keyboard.nextInt();
         Gridpi calculator = new Gridpi(response);
+        System.out.println("\n");
+        System.out.print("Pi = ");
+        System.out.print(calculator.getPi());
     }
 }
