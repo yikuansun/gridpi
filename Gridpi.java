74ab8ade28a04bd2ca4b75e7783c61e3a155aa86
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-12-21 11:24:24 -0500

    Create Gridpi.java

---
+++ Gridpi.java
@@ -0,0 +1,11 @@
+public class Gridpi {
+    private int gaps;
+
+    public Gridpi(int gaps) {
+        this.setGaps(gaps);
+    }
+
+    public void setGaps(int gaps) {
+        this.gaps = gaps;
+    }
+}
